186dbc97ea2b18d68bb19aa70d6241de42639ad3
JordanL35 <jordan.lequertier@orange.fr>
2026-03-28 03:31:05 +0100

    Update ScoreSauvegarder.php

--- Web/ScoreSauvegarder.php
+++ Web/ScoreSauvegarder.php
@@ -6,26 +6,24 @@ require_once("../Form/config.php");
 
 // Lire les données JSON envoyées par JS
 $data = json_decode(file_get_contents("php://input"), true);
-/////////////////////////////////////////////////////////
+
 // Sécurité
 if (!$data) {
     echo "Erreur : aucune donnée reçue";
     exit;
 }
-////////////////////////////////////////////////////////
+
 $id_user = $data['id_user'] ?? null;
 $id_image = $data['id_image'] ?? null;
 $temps = $data['temps'] ?? null;
 $erreurs = $data['erreurs'] ?? null;
 $score_final = $data['score_final'] ?? null;
 
-/////////////////////////////////////////////////////////
 //  Vérification
 if ($id_user === null) {
     echo "Erreur : id_user manquant";
     exit;
 }
-//////////////////////////////////////////////////////////
 
 // Préparer la requête avec PDO
 $stmt = $db->prepare("
